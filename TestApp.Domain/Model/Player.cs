579ec58072b6f183f829fb57d7cf02b40972d3b7
Nikita <kolchin-03@inbox.ru>
2022-09-18 23:44:47 +0500

    Создание слоя Domain Создание модели игрока

---
+++ TestApp.Domain/Model/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Domain.Model
+{
+    public class Player
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+        public string BirthDate { get; set; }
+        public string TeamName { get; set; }
+        public string County { get; set; }
+    }
+}
